b7351ce959e6411836620c2ba58a9dea2b04c03f
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2021-10-25 15:27:14 +0200

    Init simulation for QAM16

--- main.py
+++ main.py
@@ -6,42 +6,52 @@ import channels
 # import plot_settings
 import modulation
 import numpy as np
-
+from numpy import log2
 
 # %%
-my_mod = modulation.QamModem(16)
-my_chan = channels.AwgnChannel(0, True)
 # my_mod.plot_constellation()
-
-tx_bits = gen_tx_bits(64 * 4)
-tx_symb = my_mod.modulate(tx_bits)
-
-fig, ax = plt.subplots()
-ax.scatter(tx_symb.real, tx_symb.imag)
-plt.show()
-
-# OFDM params
+constel_size = 16
 n_sub_carr = 64
 cyclic_prefix_len = int(64 * 0.25)
+n_bits_per_ofdm_sym = int(log2(constel_size) * n_sub_carr)
 
-tx_ofdm_symbol = modulation.tx_ofdm_symbol(tx_symb, n_sub_carr, cyclic_prefix_len)
+tot_bits = n_bits_per_ofdm_sym * 1000
+tot_err = 0
 
-rx_ofdm_symbol = my_chan.propagate(tx_ofdm_symbol)
+snrs = np.arange(0, 22, 2)
+print("SNR array:", snrs)
+bers = np.zeros([len(snrs)])
 
-rx_symb = modulation.rx_ofdm_symbol(rx_ofdm_symbol, n_sub_carr, cyclic_prefix_len)
+ofdm_symbols = int(tot_bits / n_bits_per_ofdm_sym)
+
+my_mod = modulation.QamModem(constel_size)
+my_chan = channels.AwgnChannel(0, True)
+
+for idx, snr in enumerate(snrs):
+    my_chan.set_snr(snr)
+    n_err = 0
+    for _ in range(ofdm_symbols):
+        tx_bits = gen_tx_bits(n_bits_per_ofdm_sym)
+        tx_symb = my_mod.modulate(tx_bits)
+        tx_ofdm_symbol = modulation.tx_ofdm_symbol(tx_symb, n_sub_carr, cyclic_prefix_len)
+        rx_ofdm_symbol = my_chan.propagate(tx_ofdm_symbol)
+        rx_symb = modulation.rx_ofdm_symbol(rx_ofdm_symbol, n_sub_carr, cyclic_prefix_len)
+        rx_bits = my_mod.demodulate(rx_symb)
+        n_bit_err = count_mismatched_bits(tx_bits, rx_bits)
+        n_err += n_bit_err
+    bers[idx] = n_err / tot_bits
+
+# %%
+
+print("BER arr:", bers)
 
 fig, ax = plt.subplots()
-ax.scatter(rx_symb.real, rx_symb.imag)
+ax.scatter(snrs, bers, label='QAM16')
+ax.set_yscale('log')
+ax.set_xlabel('SNR [dB]')
+ax.set_ylabel("BER")
+ax.grid()
+ax.legend()
 plt.show()
-
-#fig, ax = plt.subplots()
-#ax.plot(tx_ofdm_symbol)
-#ax.plot(rx_ofdm_symbol)
-#plt.show()
-
-rx_bits = my_mod.demodulate(rx_symb)
-
-n_bit_err = count_mismatched_bits(tx_bits, rx_bits)
-print("Number of bit errors: %d" % n_bit_err)
 
 print("Finished exectution!")
