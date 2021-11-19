ff385eccecc0a1bdb2efdee34e4c9559c4dc9903
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2021-11-19 22:37:07 +0100

    changed script running structure

--- main.py
+++
@@ -1,3 +0,0 @@
-# simple wrapper to switch between main scripts and run them using IDE
-if __name__ == "__main__":
-    import main_mrt_precoding
--- main_mrt_precoding.py
+++ main_mrt_precoding.py
@@ -1,1 +1,17 @@
 # antenna array evaluation
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import welch
+import time
+
+from utilities import count_mismatched_bits, snr_to_ebn0, ebn0_to_snr, to_db, ofdm_avg_sample_pow
+import channels
+import modulation
+import impairments
+
+from plot_settings import set_latex_plot_style
+
+set_latex_plot_style()
+
+print("Multi antenna processing init!")
