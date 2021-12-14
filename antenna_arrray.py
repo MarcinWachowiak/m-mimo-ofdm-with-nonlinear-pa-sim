Fixed rayleigh channel precoding normalization, added -1 reflection coeffficient to two path

antenna_arrray.py
@@ -74,11 +74,11 @@ class LinearArray:
                     angle_of_elev_rad = np.arctan(dim_ratio)
 
                     second_path_len = rx_transceiver.cord_z / np.sin(angle_of_elev_rad)
-                    # TODO: add optional reflection coefficient
                     first_path_len = tx_transceiver.cord_z / np.sin(angle_of_elev_rad)
-                    # Change of phase, att at reflection point?
-                    sec_fd_shift_mat = np.exp(
-                        2j * np.pi * (first_path_len + second_path_len) * (sig_freq_vals / scp.constants.c))
+
+                    reflection_coeff = -1.0
+                    sec_fd_shift_mat = reflection_coeff * np.exp(2j * np.pi * (first_path_len + second_path_len) *
+                                              (sig_freq_vals / scp.constants.c))
                     two_path_fd_chan = np.add(los_fd_shift_mat, sec_fd_shift_mat)
                     # normalize to exclude amplification/attenuation
                     two_path_fd_chan_normalized = np.exp(1j * np.angle(two_path_fd_chan))
@@ -103,7 +103,10 @@ class LinearArray:
                 precoding_mat_fd[idx, :] = precoding_vec_fd
         else:
             # set precoding vector based on provided channel mat coefficients
-            precoding_mat_fd = np.conjugate(channel_fd_mat)
+            channel_fd_mat_conjungate = np.conjugate(channel_fd_mat)
+            # normalize rayleigh channel precoding coefficients
+            precoding_mat_fd = np.exp(1j * np.angle(channel_fd_mat_conjungate))
+
         # apply precoding vectors to each tx node from matrix
         for idx, tx_transceiver in enumerate(self.array_elements):
             # select coefficients based on carrier frequencies

channel.py
@@ -180,7 +180,9 @@ class AwgnMisoTwoPathTdFd(Channel):
             sec_distances[idx] = incident_path_len + reflected_path_len
 
         los_fd_shift_mat = np.exp(2j * np.pi * np.outer(los_distances, sig_freq_vals) / scp.constants.c)
-        sec_fd_shift_mat = np.exp(2j * np.pi * np.outer(sec_distances, sig_freq_vals) / scp.constants.c)
+        # TODO: include detailed calculation of reflection coefficient
+        reflection_coeff = -1.0
+        sec_fd_shift_mat = reflection_coeff * np.exp(2j * np.pi * np.outer(sec_distances, sig_freq_vals) / scp.constants.c)
 
         if not skip_attenuation:
             los_fd_att_mat = np.sqrt(np.power(10, (tx_ant_gains[:, np.newaxis] + rx_transceiver.rx_ant_gain_db) / 10)) \

corrector.py
@@ -17,9 +17,10 @@ class CncReceiver():
         in_sig_fd = np.concatenate((in_sig_fd[-n_sub_carr // 2:], in_sig_fd[1:(n_sub_carr // 2) + 1]))
         # allow a fixed number of iterations
         for iter_idx in range(n_iters):
+            # skip estimate subtraction for first iteration
             if iter_idx != 0:
                 in_sig_fd = in_sig_fd - distortion_estimate_fd
-                # skip estimate subtraction
+
 
             # perform detection with corrected RX constellation - get symbols
             rx_symbols = self.modem.symbol_detection(in_sig_fd/self.modem.alpha)
@@ -36,9 +37,9 @@ class CncReceiver():
             clipped_ofdm_sym_td = self.impairment.process(ofdm_sym_td)
 
             # simulate OFDM receive
-            ofdm_sym_fd = torch.fft.fft(torch.from_numpy(clipped_ofdm_sym_td), norm="ortho").numpy()
+            clipped_ofdm_sym_fd = torch.fft.fft(torch.from_numpy(clipped_ofdm_sym_td), norm="ortho").numpy()
 
-            rx_symbols_estimate = np.concatenate((ofdm_sym_fd[-n_sub_carr // 2:], ofdm_sym_fd[1:(n_sub_carr // 2) + 1])) / np.sqrt(upsample_factor)
+            rx_symbols_estimate = np.concatenate((clipped_ofdm_sym_fd[-n_sub_carr // 2:], clipped_ofdm_sym_fd[1:(n_sub_carr // 2) + 1])) / np.sqrt(upsample_factor)
 
             # calculate distortion estimate
             distortion_estimate_fd = rx_symbols_estimate - (rx_symbols * self.modem.alpha)

main_mrt_precoding_miso_awgn_2path.py
@@ -15,6 +15,7 @@ import modulation
 import transceiver
 from plot_settings import set_latex_plot_style
 from utilities import to_db, pts_on_circum, pts_on_semicircum
+from matplotlib.ticker import MaxNLocator
 
 set_latex_plot_style()
 # %%
@@ -29,7 +30,7 @@ my_mod = modulation.OfdmQamModem(constel_size=64, n_fft=4096, n_sub_carr=1024, c
 my_distortion = impairment.SoftLimiter(ibo_db=5, avg_symb_pow=my_mod.ofdm_avg_sample_pow())
 my_tx = transceiver.Transceiver(modem=my_mod, impairment=my_distortion, center_freq=int(3.5e9),
                                 carrier_spacing=int(15e3))
-my_rx = transceiver.Transceiver(modem=my_mod, impairment=None, cord_x=150, cord_y=150, cord_z=1.5,
+my_rx = transceiver.Transceiver(modem=my_mod, impairment=None, cord_x=212, cord_y=212, cord_z=1.5,
                                 center_freq=int(3.5e9), carrier_spacing=int(15e3))
 my_rx.modem.correct_constellation(ibo_db=my_tx.impairment.ibo_db)
 
@@ -38,10 +39,10 @@ plot_full_circle = False
 
 if plot_full_circle:
     n_points = 360
-    rx_points = pts_on_circum(r=100, n=n_points)
+    rx_points = pts_on_circum(r=300, n=n_points)
 else:
     n_points = 180
-    rx_points = pts_on_semicircum(r=100, n=n_points)
+    rx_points = pts_on_semicircum(r=300, n=n_points)
 
 radian_vals = np.radians(np.linspace(0, n_points, n_points + 1))
 
@@ -52,7 +53,7 @@ n_snapshots = 10
 # %%
 # plot PSD for chosen point/angle
 point_idx_psd = 78
-n_ant_vec = [1, 2, 4, 8]
+n_ant_vec = [16, 32, 64, 128]
 
 desired_psd_at_angle_lst = []
 distortion_psd_at_angle_lst = []
@@ -67,7 +68,7 @@ for n_ant in n_ant_vec:
                                           wav_len_spacing=0.5, cord_x=0, cord_y=0, cord_z=15)
     my_miso_chan = channel.AwgnMisoTwoPathTdFd(n_inputs=my_array.n_elements, snr_db=10, is_complex=True, seed=1234)
 
-    my_rx.set_position(cord_x=150, cord_y=150, cord_z=1.5)
+    my_rx.set_position(cord_x=212, cord_y=212, cord_z=1.5)
     my_array.set_precoding_single_point(rx_transceiver=my_rx, exact=True, two_path=True)
 
     psd_at_angle_desired = np.empty(radian_vals.shape)
@@ -144,7 +145,8 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax1.set_title("Desired signal PSD at angle [dB]")
 ax1.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax1.grid(True)
-plt.savefig("figs/desired_signal_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+# ax1.xaxis.set_major_locator(MaxNLocator(6))
+plt.savefig("figs/desired_signal_beampattern_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -165,7 +167,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax2.set_title("Distortion signal PSD at angle [dB]")
 ax2.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax2.grid(True)
-plt.savefig("figs/distortion_signal_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/distortion_signal_beampattern_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -188,7 +190,7 @@ ax3.plot(radian_vals, distortion_psd_at_angle_lst[sel_idx], label="Distortion", 
 ax3.set_title("Power spectral density at angle [dB]")
 ax3.legend(title="N antennas = %d, signals:" % n_ant_vec[sel_idx], ncol=2, loc='lower center')
 ax3.grid(True)
-plt.savefig("figs/desired_vs_distortion_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/desired_vs_distortion_beampattern_ibo%d_%dant.png" %(my_tx.impairment.ibo_db, np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -208,7 +210,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax4.set_title("Signal to distortion ratio at angle [dB]")
 ax4.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax4.grid(True)
-plt.savefig("figs/sdr_at_angle_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/sdr_at_angle_polar_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -227,7 +229,7 @@ ax5.set_ylabel("Power [dB]")
 ax5.legend(title="IBO = %d [dB]" % my_tx.impairment.ibo_db)
 ax5.grid()
 plt.tight_layout()
-plt.savefig("figs/psd_at_angle_%d_deg_ibo%d.png" % (point_idx_psd, my_tx.impairment.ibo_db), dpi=600,
+plt.savefig("figs/psd_at_angle_%ddeg_ibo%d_ant%d.png" % (point_idx_psd, my_tx.impairment.ibo_db, np.max(n_ant_vec)), dpi=600,
             bbox_inches='tight')
 plt.show()
 

main_mrt_precoding_miso_awgn_los.py
@@ -37,10 +37,10 @@ plot_full_circle = False
 
 if plot_full_circle:
     n_points = 360
-    rx_points = pts_on_circum(r=100, n=n_points)
+    rx_points = pts_on_circum(r=300, n=n_points)
 else:
     n_points = 180
-    rx_points = pts_on_semicircum(r=100, n=n_points)
+    rx_points = pts_on_semicircum(r=300, n=n_points)
 
 radian_vals = np.radians(np.linspace(0, n_points, n_points + 1))
 
@@ -65,7 +65,7 @@ for n_ant in n_ant_vec:
     my_array = antenna_arrray.LinearArray(n_elements=n_ant, transceiver=my_tx, center_freq=int(3.5e9),
                                           wav_len_spacing=0.5,
                                           cord_x=0, cord_y=0, cord_z=15)
-    my_rx.set_position(cord_x=30, cord_y=30, cord_z=1.5)
+    my_rx.set_position(cord_x=212, cord_y=212, cord_z=1.5)
     my_array.set_precoding_single_point(rx_transceiver=my_rx, exact=True)
 
     psd_at_angle_desired = np.empty(radian_vals.shape)
@@ -81,11 +81,11 @@ for n_ant in n_ant_vec:
             arr_tx_sig, clean_sig_mat = my_array.transmit(in_bits=tx_bits, return_both=True)
 
             rx_sig = my_miso_chan.propagate(tx_transceivers=my_array.array_elements, rx_transceiver=my_rx,
-                                            in_sig_mat=arr_tx_sig, skip_noise=True, skip_attenuation=True)
+                                            in_sig_mat=arr_tx_sig, skip_noise=True, skip_attenuation=False)
             rx_sig = torch.fft.ifft(torch.from_numpy(rx_sig), norm="ortho").numpy()
 
             clean_rx_sig = my_miso_chan.propagate(tx_transceivers=my_array.array_elements, rx_transceiver=my_rx,
-                                                  in_sig_mat=clean_sig_mat, skip_noise=True, skip_attenuation=True)
+                                                  in_sig_mat=clean_sig_mat, skip_noise=True, skip_attenuation=False)
             clean_rx_sig = torch.fft.ifft(torch.from_numpy(clean_rx_sig), norm="ortho").numpy()
 
             # calculate PSD at point for the last value of N antennas
@@ -141,7 +141,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax1.set_title("Desired signal PSD at angle [dB]")
 ax1.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax1.grid(True)
-plt.savefig("figs/desired_signal_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/desired_signal_beampattern_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -162,7 +162,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax2.set_title("Distortion signal PSD at angle [dB]")
 ax2.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax2.grid(True)
-plt.savefig("figs/distortion_signal_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/distortion_signal_beampattern_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -185,7 +185,7 @@ ax3.plot(radian_vals, distortion_psd_at_angle_lst[sel_idx], label="Distortion", 
 ax3.set_title("Power spectral density at angle [dB]")
 ax3.legend(title="N antennas = %d, signals:" % n_ant_vec[sel_idx], ncol=2, loc='lower center')
 ax3.grid(True)
-plt.savefig("figs/desired_vs_distortion_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/desired_vs_distortion_beampattern_ibo%d_%dant.png" %(my_tx.impairment.ibo_db, np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -205,7 +205,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax4.set_title("Signal to distortion ratio at angle [dB]")
 ax4.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax4.grid(True)
-plt.savefig("figs/sdr_at_angle_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/sdr_at_angle_polar_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -224,7 +224,7 @@ ax5.set_ylabel("Power [dB]")
 ax5.legend(title="IBO = %d [dB]" % my_tx.impairment.ibo_db)
 ax5.grid()
 plt.tight_layout()
-plt.savefig("figs/psd_at_angle_%d_deg_ibo%d.png" % (point_idx_psd, my_tx.impairment.ibo_db), dpi=600,
+plt.savefig("figs/psd_at_angle_%ddeg_ibo%d_ant%d.png" % (point_idx_psd, my_tx.impairment.ibo_db, np.max(n_ant_vec)), dpi=600,
             bbox_inches='tight')
 plt.show()
 

main_mrt_precoding_miso_rayleigh.py
@@ -39,10 +39,10 @@ plot_full_circle = False
 
 if plot_full_circle:
     n_points = 360
-    rx_points = pts_on_circum(r=100, n=n_points)
+    rx_points = pts_on_circum(r=300, n=n_points)
 else:
     n_points = 180
-    rx_points = pts_on_semicircum(r=100, n=n_points)
+    rx_points = pts_on_semicircum(r=300, n=n_points)
 
 radian_vals = np.radians(np.linspace(0, n_points, n_points + 1))
 
@@ -71,7 +71,7 @@ for n_ant in n_ant_vec:
                                           cord_x=0, cord_y=0, cord_z=15)
     my_miso_chan = channel.RayleighMisoTdFd(n_inputs=my_array.n_elements, fd_samp_size=my_tx.modem.n_fft, snr_db=10,
                                         is_complex=True, seed=1234)
-    my_rx.set_position(cord_x=30, cord_y=30, cord_z=1.5)
+    my_rx.set_position(cord_x=212, cord_y=212, cord_z=1.5)
     chan_mat_at_point = my_miso_chan.get_channel_coeffs()
     my_array.set_precoding_single_point(rx_transceiver=my_rx, channel_fd_mat=chan_mat_at_point)
 
@@ -153,7 +153,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax1.set_title("Desired signal PSD at angle [dB]")
 ax1.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax1.grid(True)
-plt.savefig("figs/desired_signal_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/desired_signal_beampattern_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -174,7 +174,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax2.set_title("Distortion signal PSD at angle [dB]")
 ax2.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax2.grid(True)
-plt.savefig("figs/distortion_signal_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/distortion_signal_beampattern_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -197,7 +197,7 @@ ax3.plot(radian_vals, distortion_psd_at_angle_lst[sel_idx], label="Distortion", 
 ax3.set_title("Power spectral density at angle [dB]")
 ax3.legend(title="N antennas = %d, signals:" % n_ant_vec[sel_idx], ncol=2, loc='lower center')
 ax3.grid(True)
-plt.savefig("figs/desired_vs_distortion_beampattern_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/desired_vs_distortion_beampattern_ibo%d_%dant.png" %(my_tx.impairment.ibo_db, np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -217,7 +217,7 @@ for idx, n_ant in enumerate(n_ant_vec):
 ax4.set_title("Signal to distortion ratio at angle [dB]")
 ax4.legend(title="N antennas:", ncol=len(n_ant_vec), loc='lower center')
 ax4.grid(True)
-plt.savefig("figs/sdr_at_angle_polar_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/sdr_at_angle_polar_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 # %%
@@ -234,7 +234,7 @@ ax5.set_ylabel("Signal to distortion ratio [dB]")
 ax5.set_title("Signal to distortion ratio at angle")
 ax5.legend(title="N antennas:")
 ax5.grid(True)
-plt.savefig("figs/sdr_at_angle_cartesian_ibo%d.png" % my_tx.impairment.ibo_db, dpi=600, bbox_inches='tight')
+plt.savefig("figs/sdr_at_angle_cartesian_ibo%d_%dto%dant_sweep.png" %(my_tx.impairment.ibo_db, np.min(n_ant_vec), np.max(n_ant_vec)), dpi=600, bbox_inches='tight')
 plt.show()
 
 
@@ -254,7 +254,7 @@ ax6.set_ylabel("Power [dB]")
 ax6.legend(title="IBO = %d [dB]" % my_tx.impairment.ibo_db)
 ax6.grid()
 plt.tight_layout()
-plt.savefig("figs/psd_at_angle_%d_deg_ibo%d.png" % (point_idx_psd, my_tx.impairment.ibo_db), dpi=600,
+plt.savefig("figs/psd_at_angle_%ddeg_ibo%d_ant%d.png" % (point_idx_psd, my_tx.impairment.ibo_db, np.max(n_ant_vec)), dpi=600,
             bbox_inches='tight')
 plt.show()
 

modulation.py
@@ -48,11 +48,7 @@ class Modem:
     # TODO: optimize symbol detection
     def symbol_detection(self, input_symbols):
         index_list = np.abs(input_symbols - self.constellation[:, None]).argmin(0)
-        demod_bits = dec2bitarray(index_list, self.n_bits_per_symbol)
-        mapfunc = np.vectorize(lambda i:
-                               self.constellation[bitarray2dec(demod_bits[i:i + self.n_bits_per_symbol])])
-        baseband_symbols = mapfunc(np.arange(0, len(demod_bits), self.n_bits_per_symbol))
-        return baseband_symbols
+        return self.constellation[index_list]
 
     def plot_constellation(self):
         fig, ax = plt.subplots()