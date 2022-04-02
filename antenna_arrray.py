Fixed multi user precoding normalization and main script, figs cleanup

antenna_arrray.py
@@ -95,6 +95,7 @@ class LinearArray:
                 tx_n_sc = tx_transceiver.modem.n_sub_carr
                 precoding_vec = precoding_mat_fd[idx, :]
                 tx_transceiver.modem.set_precoding(precoding_vec)
+
         # multiple user precoding
         else:
             n_users = len(channel_mat_fd)
@@ -109,6 +110,14 @@ class LinearArray:
             #     ant_norm_mat[usr_idx, :] = np.sum(np.power(np.abs(sc_channel_mat_fd), 2), axis=1)
             # ant_norm_coeff_vec = 1/np.sqrt(np.sum(ant_norm_mat, axis=0))
 
+            # calculate the normalizing factor K
+            usrs_vects = np.empty((n_users, tx_n_sc))
+            for usr_idx, usr_chan_mat_fd in enumerate(channel_mat_fd):
+                sc_channel_mat_fd = np.concatenate(
+                    (usr_chan_mat_fd[:, 1:(tx_n_sc // 2) + 1], usr_chan_mat_fd[:, -tx_n_sc // 2:]), axis=1)
+                usrs_vects[usr_idx, :] = np.sum(np.power(np.abs(sc_channel_mat_fd), 2), axis=0)
+            nsc_power_normalzing_vec = np.sqrt(np.sum(usrs_vects, axis=0))
+
             for usr_idx, usr_chan_mat_fd in enumerate(channel_mat_fd):
                 sc_channel_mat_fd = np.concatenate(
                     (usr_chan_mat_fd[:, 1:(tx_n_sc // 2) + 1], usr_chan_mat_fd[:, -tx_n_sc // 2:]), axis=1)
@@ -117,8 +126,7 @@ class LinearArray:
                 if mr_precoding is True:
                     # normalize the precoding vector in regard to number of antennas and power
                     # equal sum of TX power MR precoding
-                    usr_precoding_mat_fd = np.divide(sc_channel_mat_fd_conjugate,
-                                                     np.sqrt(np.sum(np.power(np.abs(sc_channel_mat_fd), 2), axis=0)))
+                    usr_precoding_mat_fd = np.divide(sc_channel_mat_fd_conjugate, nsc_power_normalzing_vec)
                     # equal sum of RX power MR precoding
                     # precoding_mat_fd = np.divide(sc_channel_mat_fd_conjugate, np.sum(np.power(np.abs(sc_channel_mat_fd), 2), axis=0))
 
@@ -128,18 +136,12 @@ class LinearArray:
                     usr_precoding_mat_fd = np.exp(1j * np.angle(sc_channel_mat_fd_conjugate))
                 precoding_mat_fd[usr_idx, :, :] = usr_precoding_mat_fd
 
-            # normalize the precoding
-            # #multiple user TX power normalization factor
-            ant_norm_mat = np.empty((n_users, self.n_elements))
-            for usr_idx, usr_chan_mat_fd in enumerate(channel_mat_fd):
-                ant_norm_mat[usr_idx, :] = np.sum(np.power(np.abs(precoding_mat_fd[usr_idx, :, :]), 2), axis=1)
-            ant_norm_coeff_vec = 1/np.sqrt(np.sum(ant_norm_mat, axis=0))
 
             # apply precoding matrix to each tx node
             for tx_idx, tx_transceiver in enumerate(self.array_elements):
                 # select coefficients based on carrier frequencies
                 mu_precoding_slice = precoding_mat_fd[:, tx_idx, :]
-                tx_transceiver.modem.set_precoding(mu_precoding_slice * ant_norm_coeff_vec[tx_idx])
+                tx_transceiver.modem.set_precoding(mu_precoding_slice)
 
     def update_distortion(self, ibo_db, avg_sample_pow):
         # calculate the avg precoding gain only for the desired signal - withing the idx range of subcarriers
@@ -161,6 +163,7 @@ class LinearArray:
             for idx, array_tx in enumerate(self.array_elements):
                 precoding_matrix[idx, :] = np.sum(array_tx.modem.precoding_mat, axis=0)
             avg_precoding_gain = np.average(np.divide(np.power(np.abs(precoding_matrix), 2), np.sum(np.power(np.abs(precoding_matrix), 2), axis=0)))
+
         # Equal total RX power precoding distortion normalization
         # sc_channel_mat = np.concatenate((channel_mat_fd[:, 1:(tx_n_sc // 2) + 1], channel_mat_fd[:, -tx_n_sc // 2:]), axis=1)
         #

main_multiple_user_precoding.py
@@ -15,7 +15,7 @@ import modulation
 import transceiver
 import utilities
 from plot_settings import set_latex_plot_style
-from utilities import to_db, pts_on_circum, pts_on_semicircum
+from utilities import to_db, pts_on_circum, pts_on_semicircum, td_signal_power
 from matplotlib.ticker import MaxNLocator
 
 set_latex_plot_style()
@@ -31,8 +31,8 @@ bit_rng = np.random.default_rng(4321)
 ibo_val_db = 5
 
 # Multiple users data
-usr_angles = [30, 90, 135]
-usr_distances = [30, 60, 150]
+usr_angles = [15, 45, 90, 160]
+usr_distances = [150, 100, 200, 175]
 n_users = len(usr_angles)
 max_point_idx = usr_angles[0]
 
@@ -64,7 +64,7 @@ n_snapshots = 10
 # %%
 # plot PSD for chosen point/angle
 point_idx_psd = 78
-n_ant_vec = [16]
+n_ant_vec = [32]
 
 desired_psd_at_angle_lst = []
 distortion_psd_at_angle_lst = []
@@ -84,8 +84,8 @@ for n_ant in n_ant_vec:
     #get channel matrix to all users
     mu_channel_mat = []
     for usr_idx, usr_angle in enumerate(usr_angles):
-        usr_pos_x = np.cos(usr_angle) * usr_distances[usr_idx]
-        usr_pos_y = np.sin(usr_angle) * usr_distances[usr_idx]
+        usr_pos_x = np.cos(np.deg2rad(usr_angle)) * usr_distances[usr_idx]
+        usr_pos_y = np.sin(np.deg2rad(usr_angle))* usr_distances[usr_idx]
 
         my_rx.set_position(cord_x=usr_pos_x, cord_y=usr_pos_y, cord_z=1.5)
         my_miso_chan.calc_channel_mat(tx_transceivers=my_array.array_elements, rx_transceiver=my_rx, skip_attenuation=False)
@@ -109,7 +109,7 @@ for n_ant in n_ant_vec:
         clean_rx_sig_accum = []
         for snap_idx in range(n_snapshots):
 
-            tx_bits = bit_rng.choice((0, 1), (n_users, my_tx.modem.n_bits_per_ofdm_sym))
+            tx_bits = np.squeeze(bit_rng.choice((0, 1), (n_users, my_tx.modem.n_bits_per_ofdm_sym)))
             arr_tx_sig_fd, clean_sig_mat_fd = my_array.transmit(in_bits=tx_bits, out_domain_fd=True, return_both=True)
             # print(utilities.td_signal_power(utilities.to_time_domain(arr_tx_sig_fd[0,:])))
 

modulation.py
@@ -117,6 +117,7 @@ class QamModem(Modem):
 @jit(nopython=True)
 def _tx_ofdm_symbol(mod_symbols, n_fft: int, n_sub_carr: int, cp_length: int):
     # generate OFDM symbol block - size given by n_sub_carr size
+
     if len(mod_symbols) != n_sub_carr:
         raise ValueError('mod_symbols length must match n_sub_carr value')
 
@@ -175,7 +176,7 @@ class OfdmQamModem(QamModem):
             if get_symbols_only:
                 return modulated_symbols
             else:
-                precoded_symbols = self.precode_symbols(modulated_symbols, self.precoding_mat)
+                precoded_symbols = np.squeeze(self.precode_symbols(modulated_symbols, self.precoding_mat))
                 return _tx_ofdm_symbol(precoded_symbols, self.n_fft, self.n_sub_carr, self.cp_len)
         else:
             modulated_symbols = np.empty((self.n_users, self.n_sub_carr), dtype=np.complex128)