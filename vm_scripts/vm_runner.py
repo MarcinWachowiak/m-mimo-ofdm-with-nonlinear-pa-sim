2e2f7c441598da691566deae116842ce0feaeda2
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2022-06-26 20:50:22 +0200

    Added VM sim runner prototype

---
+++ vm_scripts/vm_runner.py
@@ -0,0 +1,29 @@
+# VM simulation runner
+import os, sys
+import time
+from datetime import datetime
+
+sys.path.append(os.getcwd())
+
+filename_str_lst = ["main_beampatterns_plotting/main_sdr_vs_ibo_vs_channel.py",
+                "main_clipping_noise_cancellation/main_miso_cnc_ber_vs_ebn0.py",
+                "main_clipping_noise_cancellation/main_miso_mcnc_ber_vs_ebn0.py"
+                "main_clipping_noise_cancellation/main_miso_cnc_ber_vs_ibo.py",
+                "main_clipping_noise_cancellation/main_miso_mcnc_ber_vs_ibo.py"
+                "main_clipping_noise_cancellation/main_miso_cnc_constant_ber_req_ebn0_vs_ibo.py",
+                "main_clipping_noise_cancellation/main_miso_mcnc_constant_ber_req_ebn0_vs_ibo.py"
+                ]
+for idx, filename_str in enumerate(filename_str_lst):
+    print("Running: %d/%d, %s" %(idx+1, len(filename_str_lst), filename_str))
+
+    start_time = time.time()
+    print("--- Start time: %s ---" % datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    # SDR VS IBO VS N_ANT VS CHANNEL
+    try:
+        os.system("python3 %s" % filename_str)
+    except:
+        print("Runtime error occurred!")
+
+    print("--- Computation time: %f ---" % (time.time() - start_time))
+
+print("Finished computation!")
