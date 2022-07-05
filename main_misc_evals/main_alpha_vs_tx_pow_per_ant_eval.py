67efa921ec305e0e0e719cf292327ed6d8984b81
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2022-07-05 21:24:23 +0200

    Changed the presentation of the alpha vs tx power (IBO)

--- main_misc_evals/main_alpha_vs_tx_pow_per_ant_eval.py
+++ main_misc_evals/main_alpha_vs_tx_pow_per_ant_eval.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+import utilities
+
 sys.path.append(os.getcwd())
 
 import copy
@@ -26,22 +28,22 @@ set_latex_plot_style()
 print("Multi antenna processing init!")
 # remember to copy objects not to avoid shared properties modifications!
 # check modifications before copy and what you copy!
-my_mod = modulation.OfdmQamModem(constel_size=64, n_fft=4096, n_sub_carr=1024, cp_len=128)
+my_mod = modulation.OfdmQamModem(constel_size=64, n_fft=4096, n_sub_carr=2048, cp_len=128)
 my_distortion = distortion.SoftLimiter(ibo_db=0, avg_samp_pow=my_mod.avg_sample_power)
 my_tx = transceiver.Transceiver(modem=copy.deepcopy(my_mod), impairment=copy.deepcopy(my_distortion),
                                 center_freq=int(3.5e9),
                                 carrier_spacing=int(15e3))
-my_rx = transceiver.Transceiver(modem=copy.deepcopy(my_mod), impairment=copy.deepcopy(my_distortion), cord_x=212,
-                                cord_y=212, cord_z=1.5,
+my_rx = transceiver.Transceiver(modem=copy.deepcopy(my_mod), impairment=copy.deepcopy(my_distortion), cord_x=500,
+                                cord_y=500, cord_z=1.5,
                                 center_freq=int(3.5e9), carrier_spacing=int(15e3))
 my_rx.correct_constellation()
 
 # %%
 n_ant_val = 64
 # averaging length
-n_ofdm_symb = 1e2
+n_ofdm_symb = 1e4
 print("N antennas values:", n_ant_val)
-ibo_val_db = 5
+ibo_val_db = 0
 print("IBO value:", ibo_val_db)
 
 # %%
@@ -99,6 +101,7 @@ for chan_idx, chan_obj in enumerate(chan_lst):
         # estimate lambda parameters for each antenna and compare in regard to the average
         lambda_numerator_vecs.append(np.multiply(tx_nsc_ofdm_symb_fd, np.conjugate(clean_nsc_ofdm_symb_fd)))
         lambda_denominator_vecs.append(np.multiply(clean_nsc_ofdm_symb_fd, np.conjugate(clean_nsc_ofdm_symb_fd)))
+
         ofdm_symb_pow_lst.append(np.sum(np.abs(clean_nsc_ofdm_symb_fd) ** 2, axis=1) / my_mod.n_fft)
         ofdm_symb_idx += 1
 
@@ -111,19 +114,22 @@ for chan_idx, chan_obj in enumerate(chan_lst):
     lambda_per_nant_per_ibo[chan_idx, :] = np.abs(lambda_num / lambda_denum)
     print("--- Computation time: %f ---" % (time.time() - start_time))
 
+ibo_per_tx = np.multiply(10, np.log10(np.divide(my_array.array_elements[0].impairment.sat_pow, tx_pow_arr)))
+
 # %%
 fig1, ax1 = plt.subplots(1, 1)
 # plot averaged power and lambda
 avg_tx_pow = my_mod.avg_sample_power * my_array.get_avg_precoding_gain()
-lambda_analytical = my_mod.calc_alpha(ibo_db=ibo_val_db)
 
-ax1.plot(tx_pow_arr[0, :], lambda_per_nant_per_ibo[0, :], '.', label="Rayleigh")
-ax1.plot(tx_pow_arr[1, :], lambda_per_nant_per_ibo[1, :], '.', label="Two-path")
-ax1.plot(tx_pow_arr[2, :], lambda_per_nant_per_ibo[2, :], '.', label="LOS")
-ax1.plot(avg_tx_pow, lambda_analytical, '.k', label="Analytical")
+ibo_range = np.linspace(np.min(ibo_per_tx), np.max(ibo_per_tx), 100)
+alpha_analytical = my_mod.calc_alpha(ibo_db=ibo_range)
+ax1.plot(ibo_per_tx[0, :], lambda_per_nant_per_ibo[0, :], '.', label="Rayleigh")
+ax1.plot(ibo_per_tx[1, :], lambda_per_nant_per_ibo[1, :], '.', label="Two-path")
+ax1.plot(ibo_per_tx[2, :], lambda_per_nant_per_ibo[2, :], '.', label="LOS")
+ax1.plot(ibo_range, alpha_analytical, '--k', label="Analytical")
 
-ax1.set_title(r"$\alpha$ coeff in relation to TX power [-]")
-ax1.set_xlabel("TX power: $AVG\, |y_{k,n}|^2$[W]")
+ax1.set_title(r"$\alpha$ coeff in relation IBO = %d [dB]" % (ibo_val_db))
+ax1.set_xlabel("IBO[dB]")
 ax1.set_ylabel(r"$\alpha$ [-]")
 ax1.grid()
 ax1.legend(title="Channel:")
@@ -133,5 +139,16 @@ plt.savefig(
     "../figs/alpha_vs_tx_power_per_ant%d_ibo%1.1f.png" % (n_ant_val, ibo_val_db),
     dpi=600, bbox_inches='tight')
 plt.show()
+
+# %%
+data_lst = []
+data_lst.append(tx_pow_arr[0, :])
+data_lst.append(tx_pow_arr[1, :])
+data_lst.append(tx_pow_arr[2, :])
+data_lst.append(lambda_per_nant_per_ibo[0, :])
+data_lst.append(lambda_per_nant_per_ibo[1, :])
+data_lst.append(lambda_per_nant_per_ibo[2, :])
+
+utilities.save_to_csv(data_lst=data_lst, filename="alpha_vs_tx_power_per_ant%d_ibo%1.1f.png" % (n_ant_val, ibo_val_db))
 
 print("Finished execution!")
