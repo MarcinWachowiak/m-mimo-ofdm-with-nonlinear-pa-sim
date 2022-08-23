a103f4e196d5402b64f9b91d07a7a3a40d9c6732
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2022-08-23 18:36:55 +0200

    restored old mimo sim parameters

--- main_mp_clipping_noise_cancellation/main_mp_miso_mcnc_ber_vs_ebn0.py
+++ main_mp_clipping_noise_cancellation/main_mp_miso_mcnc_ber_vs_ebn0.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
     # %%
     # parameters
-    n_ant_arr = [1]
+    n_ant_arr = [64]
     ibo_arr = [0]
     ebn0_step = [1]
     cnc_n_iter_lst = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -80,7 +80,7 @@ if __name__ == '__main__':
         my_miso_rayleigh_chan = channel.RayleighMisoFd(tx_transceivers=my_array.array_elements,
                                                        rx_transceiver=my_standard_rx,
                                                        seed=1234)
-        chan_lst = [my_miso_rayleigh_chan]
+        chan_lst = [my_miso_los_chan, my_miso_two_path_chan, my_miso_rayleigh_chan]
         my_noise = noise.Awgn(snr_db=10, seed=1234)
 
         for my_miso_chan in chan_lst:
