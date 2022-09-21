1f448fd8e26465801db982522ef668ce1bcca6ce
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2022-09-21 20:56:17 +0200

    tmp results

--- vm_scripts/vm_runner.py
+++ vm_scripts/vm_runner.py
@@ -8,7 +8,7 @@ sys.path.append(os.getcwd())
 
 filename_str_lst = [
     "main_mp_clipping_noise_cancellation/main_mp_miso_cnc_ber_vs_ebn0.py",
-    "main_mp_clipping_noise_cancellation/main_mp_miso_cnc_ber_vs_ibo.py",
+    # "main_mp_clipping_noise_cancellation/main_mp_miso_cnc_ber_vs_ibo.py",
 
     "main_mp_clipping_noise_cancellation/main_mp_miso_mcnc_ber_vs_ebn0.py",
     "main_mp_clipping_noise_cancellation/main_mp_miso_mcnc_ber_vs_ibo.py",
