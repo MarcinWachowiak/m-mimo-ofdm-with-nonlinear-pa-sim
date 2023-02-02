0b6981e4d4da6cc52f52012d19e04b0a47a4036f
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2023-02-02 15:41:05 +0100

    minor update

--- final_plots/ber_ebn0_w_ldpc.py
+++ final_plots/ber_ebn0_w_ldpc.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import utilities
 from plot_settings import set_latex_plot_style
 
-set_latex_plot_style(use_tex=False, fig_width_in=5)
+set_latex_plot_style(use_tex=True, fig_width_in=3.5)
 
 cnc_n_iter_lst = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 sel_cnc_iter_val = [0, 1, 2, 5]
@@ -21,14 +21,15 @@ n_ant_val = 64
 ibo_val_db = 0
 constel_size = 64
 
-ebn0_step = 1
+ebn0_step = 0.25
 my_miso_chan = "los"
 
 code_rate_str_lst = ["1/3",  "2/3"]
-ebn0_bounds_arr = [[-5.0, 10.1], [0.0, 20.1]]
+ebn0_bounds_arr = [[-5.0, 5.1], [0.0, 15.1]]
 
 ebn0_arr_lst = []
 cnc_ber_per_ldpc_lst = []
+mcnc_ber_per_ldpc_lst = []
 for code_idx, code_rate_str in enumerate(code_rate_str_lst):
     cnc_filename_str = "ldpc_%s_ber_vs_ebn0_cnc_%s_nant%d_ibo%d_ebn0_min%d_max%d_step%1.2f_niter%s" % (
         code_rate_str.replace('/', '_'), my_miso_chan, n_ant_val, ibo_val_db, ebn0_bounds_arr[code_idx][0], ebn0_bounds_arr[code_idx][1], ebn0_step,
@@ -39,14 +40,18 @@ for code_idx, code_rate_str in enumerate(code_rate_str_lst):
 
     ebn0_arr_lst.append(cnc_ebn0_arr)
     cnc_ber_per_ldpc_lst.append(cnc_ber_per_dist)
-    # mcnc_filename_str = "ber_vs_ebn0_mcnc_%s_nant%d_ibo%d_ebn0_min%d_max%d_step%1.2f_niter%s" % (
-    #     my_miso_chan, n_ant_val, ibo_val_db, ebn0_bounds_arr[code_idx][0], ebn0_bounds_arr[code_idx][1], ebn0_step,
-    #     '_'.join([str(val) for val in cnc_n_iter_lst[1:]]))
-    # mcnc_data_lst = utilities.read_from_csv(filename=mcnc_filename_str)
-    # mcnc_ebn0_arr = mcnc_data_lst[0]
-    # mcnc_ber_per_dist = mcnc_data_lst[1:]
+
+    mcnc_filename_str = "ldpc_%s_ber_vs_ebn0_mcnc_%s_nant%d_ibo%d_ebn0_min%d_max%d_step%1.2f_niter%s" % (
+        code_rate_str.replace('/', '_'), my_miso_chan, n_ant_val, ibo_val_db, ebn0_bounds_arr[code_idx][0], ebn0_bounds_arr[code_idx][1], ebn0_step,
+        '_'.join([str(val) for val in cnc_n_iter_lst[1:]]))
+    mcnc_data_lst = utilities.read_from_csv(filename=mcnc_filename_str)
+    mcnc_ebn0_arr = mcnc_data_lst[0]
+    mcnc_ber_per_dist = mcnc_data_lst[1:]
+    mcnc_ber_per_ldpc_lst.append(mcnc_ber_per_dist)
+
 
 # %%
+
 fig1, ax1 = plt.subplots(1, 1)
 ax1.set_yscale('log', base=10)
 
@@ -55,22 +60,31 @@ CB_color_cycle = ['#006BA4', '#FF800E', '#ABABAB', '#595959', '#5F9ED1', '#C8520
 code_marker = ['*', '^']
 for code_idx, code_rate_str in enumerate(code_rate_str_lst):
     color_idx = 1
-    ax1.plot(ebn0_arr_lst[code_idx], cnc_ber_per_ldpc_lst[code_idx][0], marker=code_marker[code_idx], fillstyle='none')
+    ax1.plot(ebn0_arr_lst[code_idx], cnc_ber_per_ldpc_lst[code_idx][0])
     for idx, cnc_iter_val in enumerate(cnc_n_iter_lst):
         if cnc_iter_val in sel_cnc_iter_val:
-            ax1.plot(ebn0_arr_lst[code_idx], cnc_ber_per_ldpc_lst[code_idx][idx + 1], "-", color=CB_color_cycle[color_idx], marker=code_marker[code_idx], fillstyle='none')
+            if cnc_iter_val == 1:
+                ax1.plot(ebn0_arr_lst[code_idx][::2], cnc_ber_per_ldpc_lst[code_idx][idx + 1][::2], "-", color=CB_color_cycle[color_idx])
+            else:
+                ax1.plot(ebn0_arr_lst[code_idx], cnc_ber_per_ldpc_lst[code_idx][idx + 1], "-", color=CB_color_cycle[color_idx])
         if cnc_iter_val in sel_cnc_iter_val or cnc_iter_val == 1:
             color_idx += 1
     plot_settings.reset_color_cycle()
 
-    # color_idx = 1
-    # ax1.plot(ebn0_arr_lst[code_idx], mcnc_ber_per_ldpc_lst[code_idx][0], "--")
-    # for idx, cnc_iter_val in enumerate(cnc_n_iter_lst):
-    #     if cnc_iter_val in sel_cnc_iter_val:
-    #         ax1.plot(ebn0_arr_lst[code_idx], mcnc_ber_per_ldpc_lst[code_idx][idx + 1], "--", color=CB_color_cycle[color_idx],
-    #                  dashes=(5, 1 + idx))
-    #     if cnc_iter_val in sel_cnc_iter_val or cnc_iter_val == 1:
-    #         color_idx += 1
+    color_idx = 1
+    for idx, cnc_iter_val in enumerate(cnc_n_iter_lst):
+        if cnc_iter_val == 0:
+            color_idx += 1
+            continue
+
+        if cnc_iter_val in sel_cnc_iter_val:
+            if cnc_iter_val == 1:
+                ax1.plot(ebn0_arr_lst[code_idx][::2], mcnc_ber_per_ldpc_lst[code_idx][idx + 1][::2], "--",
+                         color=CB_color_cycle[color_idx])
+            else:
+                ax1.plot(ebn0_arr_lst[code_idx], mcnc_ber_per_ldpc_lst[code_idx][idx + 1], "--", color=CB_color_cycle[color_idx])
+        if cnc_iter_val in sel_cnc_iter_val or cnc_iter_val == 1:
+            color_idx += 1
 
 import matplotlib.lines as mlines
 
@@ -95,17 +109,20 @@ for ite_idx, ite_val in enumerate(cnc_n_iter_lst):
 leg1 = plt.legend(handles=n_ite_legend, title="I iterations:", loc="lower left", ncol=1, framealpha=0.9)
 plt.gca().add_artist(leg1)
 
-code_leg_lst = []
-for code_idx, code_rate_str in enumerate(code_rate_str_lst):
-    leg_obj = mlines.Line2D([0], [0], linestyle='none', marker=code_marker[code_idx], fillstyle="none", color='k', label=code_rate_str)
-    code_leg_lst.append(leg_obj)
+ax1.text(0.54, 0.7, '1/3', verticalalignment='center', horizontalalignment='center', transform=ax1.transAxes, fontsize=11)
+ax1.text(0.90, 0.7, '2/3', verticalalignment='center', horizontalalignment='center', transform=ax1.transAxes, fontsize=11)
 
-leg2 = plt.legend(handles=code_leg_lst, title="LDPC:", loc="upper left", framealpha=0.9, bbox_to_anchor=(0.004, 0.80))
-plt.gca().add_artist(leg2)
+# code_leg_lst = []
+# for code_idx, code_rate_str in enumerate(code_rate_str_lst):
+#     leg_obj = mlines.Line2D([0], [0], linestyle='none', marker=code_marker[code_idx], fillstyle="none", color='k', label=code_rate_str)
+#     code_leg_lst.append(leg_obj)
+#
+# leg2 = plt.legend(handles=code_leg_lst, title="LDPC:", loc="upper left", framealpha=0.9, bbox_to_anchor=(0.004, 0.80))
+# plt.gca().add_artist(leg2)
 
 cnc_leg = mlines.Line2D([0], [0], linestyle='-', color='k', label='CNC')
 mcnc_leg = mlines.Line2D([0], [0], linestyle='--', color='k', label='MCNC')
-ax1.legend(handles=[cnc_leg, mcnc_leg], loc="upper left", framealpha=0.9, bbox_to_anchor=(0.004, 0.58))
+ax1.legend(handles=[cnc_leg, mcnc_leg], loc="upper left", framealpha=0.9, bbox_to_anchor=(0.000, 0.92))
 
 
 # cnc_leg = mlines.Line2D([0], [0], linestyle='-', color='k', label='CNC')
@@ -115,8 +132,8 @@ ax1.legend(handles=[cnc_leg, mcnc_leg], loc="upper left", framealpha=0.9, bbox_t
 
 # ax1.set_title("BER vs Eb/N0, %s, CNC, QAM %d, N ANT = %d, IBO = %d [dB]" % (
 #     my_miso_chan, constel_size, n_ant_val, ibo_val_db))
-ax1.set_xlim([-5, 15])
-# ax1.set_ylim([1e-5, 3e-1])
+ax1.set_xlim([-10, 15])
+ax1.set_ylim([1e-5, 5e-1])
 
 ax1.grid(which='major', linestyle='-')
 # ax1.grid(which='minor', linestyle='--')
@@ -129,7 +146,7 @@ filename_str = "ldpc_ber_vs_ebn0_%s_nant%d_ibo%d_ebn0_min%d_max%d_step%1.2f_nite
     '_'.join([str(val) for val in sel_cnc_iter_val[1:]]))
 # timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 # filename_str += "_" + timestamp
-plt.savefig("../figs/%s.png" % filename_str, dpi=600, bbox_inches='tight')
+plt.savefig("../figs/%s.pdf" % filename_str, dpi=600, bbox_inches='tight')
 plt.show()
 
 print("Finished execution!")
--- final_plots/berin_berout_vs_ibo.py
+++ final_plots/berin_berout_vs_ibo.py
@@ -16,10 +16,9 @@ from plot_settings import set_latex_plot_style
 set_latex_plot_style(use_tex=False, fig_width_in=5)
 
 cnc_n_iter_lst = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-sel_cnc_iter_val = [1, 2, 5]
+sel_cnc_iter_val = [0, 1, 2, 3]
 
 n_ant_val = 64
-ebn0_db = 15
 constel_size = 64
 
 my_miso_chan = "los"
@@ -29,6 +28,8 @@ ibo_step = 0.5
 
 cnc_bers_per_snr_lst = []
 cnc_ibo_array_lst = []
+mcnc_bers_per_snr_lst = []
+mcnc_ibo_array_lst = []
 no_dist_ber_limit = []
 snr_lst = [10, 15, 20, 1000]
 sel_snr_val_lst = [10, 15, 20, 1000]
@@ -42,6 +43,17 @@ for snr_idx, snr_val in enumerate(snr_lst):
     cnc_bers_per_snr_lst.append(np.array(np.transpose(np.vstack(cnc_bers_per_ibo))))
     cnc_ibo_array_lst.append(cnc_ibo_arr)
     no_dist_ber_limit.append(snr_ber_limit)
+
+    mcnc_filename_str = "ber_vs_ibo_mcnc_los_nant64_ebn0_%d_ibo_min%d_max%d_step%1.2f_niter1_2_3_4_5_6_7_8" % (snr_val, ibo_min, ibo_max, ibo_step)
+    mcnc_data_lst = utilities.read_from_csv(filename=mcnc_filename_str)
+    mcnc_ibo_arr = mcnc_data_lst[0]
+    # snr_ber_limit = np.average(cnc_data_lst[1])
+    mcnc_bers_per_ibo = mcnc_data_lst[2:]
+    mcnc_bers_per_snr_lst.append(np.array(np.transpose(np.vstack(mcnc_bers_per_ibo))))
+    mcnc_ibo_array_lst.append(mcnc_ibo_arr)
+    # no_dist_ber_limit.append(snr_ber_limit)
+
+
 
 
 # %%
@@ -57,11 +69,14 @@ CB_color_cycle = ['#006BA4', '#FF800E', '#ABABAB', '#595959', '#5F9ED1', '#C8520
 for snr_idx, snr_val in enumerate(snr_lst):
     color_idx = 1
     if snr_val in sel_snr_val_lst:
-        for ite_idx, ite_val in enumerate(cnc_n_iter_lst[1:]):
+        for ite_idx, ite_val in enumerate(cnc_n_iter_lst):
             if ite_val in sel_cnc_iter_val:
-                ax1.plot(cnc_bers_per_snr_lst[snr_idx][ite_idx], cnc_bers_per_snr_lst[snr_idx][ite_idx+1], "-", color=CB_color_cycle[color_idx])
+                ax1.plot(cnc_bers_per_snr_lst[snr_idx][ite_idx], cnc_bers_per_snr_lst[snr_idx][ite_idx + 1], "-", color=CB_color_cycle[color_idx])
+                ax1.plot(mcnc_bers_per_snr_lst[snr_idx][ite_idx], mcnc_bers_per_snr_lst[snr_idx][ite_idx + 1], "--", color=CB_color_cycle[color_idx])
                 color_idx += 1
     plot_settings.reset_color_cycle()
+
+
 
 
 import matplotlib.lines as mlines
@@ -85,15 +100,15 @@ for ite_idx, ite_val in enumerate(cnc_n_iter_lst):
 leg1 = plt.legend(handles=n_ite_legend, title="I iterations:", loc="upper left", ncol=2, framealpha=0.9)
 plt.gca().add_artist(leg1)
 
-cnc_leg = mlines.Line2D([0], [0], linestyle='-', color='k', label='MCNC no noise')
-mcnc_leg = mlines.Line2D([0], [0], linestyle='--', color='k', label='MCNC Eb/N0: 15 dB')
+cnc_leg = mlines.Line2D([0], [0], linestyle='-', color='k', label='CNC')
+mcnc_leg = mlines.Line2D([0], [0], linestyle='--', color='k', label='MCNC')
 ax1.legend(handles=[cnc_leg, mcnc_leg], loc="lower right", framealpha=0.9)
 # plt.gca().add_artist(leg2)
 
 ax1.set_xlabel("BER in [-]")
 ax1.set_ylabel("BER out [-]")
-# ax1.set_xlim([1e-4, 4e-1])
-# ax1.set_ylim([1e-4, 4e-1])
+# ax1.set_xlim([1e-5, 4e-1])
+# ax1.set_ylim([1e-5, 4e-1])
 
 pt1 = ax1.get_ylim()
 pt2 = ax1.get_xlim()
@@ -102,19 +117,19 @@ ax1.plot([pt1[0], pt2[1]], [pt1[0], pt2[1]], color='k', linewidth=1, label="No B
 
 for snr_idx, snr_val in enumerate(snr_lst):
     if snr_val in sel_snr_val_lst:
-        ax1.scatter(no_dist_ber_limit[snr_idx], no_dist_ber_limit[snr_idx], color='k', marker='o', zorder=3)
+        ax1.scatter(no_dist_ber_limit[snr_idx], no_dist_ber_limit[snr_idx], color=CB_color_cycle[0], marker='o', zorder=3)
 
 ax1.grid(which='major', linestyle='-')
 # ax1.grid(which='minor', linestyle='--')
 
-ax1.annotate('Greater IBO \n (lower distortion)', xy=(6e-4, 1e-2),  xycoords='data',
-            xytext=(4e-3, 1e-2), horizontalalignment="center", verticalalignment="center", textcoords='data',
-            arrowprops=dict(facecolor='black', arrowstyle='->'))
+# ax1.annotate('Greater IBO \n (lower distortion)', xy=(6e-4, 1e-2),  xycoords='data',
+#             xytext=(4e-3, 1e-2), horizontalalignment="center", verticalalignment="center", textcoords='data',
+#             arrowprops=dict(facecolor='black', arrowstyle='->'))
 
 plt.tight_layout()
 
-filename_str = "berout_vs_berin_per_single_ite_%s_nant%d_ebn0%d_ibo_min%d_max%d_step%1.2f_niter%s" % (
-    my_miso_chan, n_ant_val, ebn0_db, ibo_min, ibo_max, ibo_step,
+filename_str = "berout_vs_berin_per_single_ite_%s_nant%d_ebn0%s_ibo_min%d_max%d_step%1.2f_niter%s" % (
+    my_miso_chan, n_ant_val, '_'.join([str(val) for val in sel_snr_val_lst[:]]), ibo_min, ibo_max, ibo_step,
     '_'.join([str(val) for val in sel_cnc_iter_val[:]]))
 # timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 # filename_str += "_" + timestamp
