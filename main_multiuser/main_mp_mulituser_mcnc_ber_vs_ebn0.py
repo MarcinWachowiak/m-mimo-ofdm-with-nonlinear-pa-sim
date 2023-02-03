235cf98111e0d2ff460269f88145648ac9b15e4d
Marcin Wachowiak <marcin.r.wachowiak@gmail.com>
2023-02-03 10:26:30 +0100

    added results of multiuser mr cnc mcnc

--- main_multiuser/main_mp_mulituser_mcnc_ber_vs_ebn0.py
+++ main_multiuser/main_mp_mulituser_mcnc_ber_vs_ebn0.py
@@ -3,9 +3,9 @@
 import os
 import sys
 
-from main_multiuser.multiuser_mp_prototype import multiuser_mcnc_parallel
-
 sys.path.append(os.getcwd())
+
+from main_multiuser.multiuser_mp_prototype import multiuser_mcnc_parallel
 
 import multiprocessing as mp
 
@@ -13,10 +13,10 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    num_cores = 10
+    num_cores = 12
     precoding_str = 'mr'
-    bits_sent_max = int(1e6)
-    n_err_min = int(1e5)
+    bits_sent_max = int(1e7 / num_cores)
+    n_err_min = int(1e6 / num_cores)
 
     seed_rng = np.random.default_rng(2137)
     proc_seed_lst = seed_rng.integers(0, high=sys.maxsize, size=(num_cores, 2))
